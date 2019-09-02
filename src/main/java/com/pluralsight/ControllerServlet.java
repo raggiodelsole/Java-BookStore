2316db7a3770919b84b0226e6d4659ca659db6dd
Raggio <radoslaw.kurowski.94@gmail.com>
2019-09-02 23:52:30 +0200

    Deleting Books From a Database- minor fixes/formatting

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -112,7 +112,7 @@ public class ControllerServlet extends HttpServlet {
 
     private void deleteBook(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        int id=Integer.parseInt(request.getParameter("id"));
+        int id = Integer.parseInt(request.getParameter("id"));
         bookDAO.deleteBook(id);
         response.sendRedirect("list");
     }

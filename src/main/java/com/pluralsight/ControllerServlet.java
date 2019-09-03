f33eb690de9b919cc8778dda8313f7fd696a45ff
Raggio <radoslaw.kurowski.94@gmail.com>
2019-09-03 22:22:27 +0200

    Showing an Edit Form

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -64,6 +64,9 @@ public class ControllerServlet extends HttpServlet {
                 case "/delete":
                     deleteBook(request, response);
                     break;
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
                 default:
                     listBooks(request, response);
                     break;
@@ -115,6 +118,13 @@ public class ControllerServlet extends HttpServlet {
         int id = Integer.parseInt(request.getParameter("id"));
         bookDAO.deleteBook(id);
         response.sendRedirect("list");
+    }
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        int id= Integer.parseInt(request.getParameter("id"));
+        Book existingBook= bookDAO.getBook(id);
+        RequestDispatcher dispatcher= request.getRequestDispatcher("/BookForm.jsp");
+        request.setAttribute("book",existingBook);
+        dispatcher.forward(request, response);
     }
 
     /**
